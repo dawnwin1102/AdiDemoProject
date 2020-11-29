967c84b8aaf15d8de075300078aba803dd99ce75
Xiaodong Mao <v-xiaomao@microsoft.com>
2020-11-30 03:41:55 +0800

    init sprng cloud gateway

---
+++ gateway-springcloud/src/main/java/com/leo/adidasdemo/gatewayspringcloud/GatewaySpringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.leo.adidasdemo.gatewayspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaySpringcloudApplication.class, args);
+	}
+
+}
---
+++ gateway-springcloud/src/test/java/com/leo/adidasdemo/gatewayspringcloud/GatewaySpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.adidasdemo.gatewayspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySpringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
